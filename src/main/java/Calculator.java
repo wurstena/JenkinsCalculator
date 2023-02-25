c450d2299b3b9c8187b6ce367032c694a6dee84e
wurstena <wurstena@byu.edu>
2023-02-24 21:45:24 -0700

    wrote intToBinaryNumber

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -70,8 +70,10 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
+        UUID randomUUID = UUID.randomUUID();
+        String newString = n + randomUUID.toString().replaceAll("_", "");
 
-        return null;
+        return newString;
     }
 
 
