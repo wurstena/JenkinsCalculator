dbd79dbc4fbada5912d4ce7cee4ffbf819e1405e
wurstena <wurstena@byu.edu>
2023-02-24 21:41:11 -0700

    writing intToBinaryNumber

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Calculator {
 
     Calculator(){
@@ -56,7 +58,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        return Integer.toBinaryString(n);
     }
 
     /*
